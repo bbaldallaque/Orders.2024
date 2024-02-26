fe648b087ac4f118c4eb6948d4670e2e967bcde1
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2024-02-25 21:55:15 -0500

    Data base Creation
    frist Entity

---
+++ Orders/Orders.Backend/Data/OrdersContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class OrdersContext(DbContextOptions<OrdersContext> options) : DbContext(options)
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
